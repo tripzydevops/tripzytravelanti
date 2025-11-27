7d05d106cb3f2cf74ae085067c6fd5ae27597d50
tripzy <tripzydevops@gmail.com>
2025-11-27 21:16:52 +0300

    Add process.env polyfill for dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     define: {
-      // 'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY), 
+      'process.env': {},
     },
     resolve: {
       alias: {
