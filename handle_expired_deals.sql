ae82a8757354c854f436adfc4064711bcba9adbc
tripzy <tripzydevops@gmail.com>
2025-12-02 17:14:49 +0300

    Update handle_expired_deals.sql

--- handle_expired_deals.sql
+++ handle_expired_deals.sql
@@ -8,16 +8,9 @@ WHERE expires_at < NOW()
     AND status NOT IN ('expired', 'rejected');
 END;
 $$ LANGUAGE plpgsql;
--- Schedule the function to run every hour
--- We use a DO block to check if the job already exists to avoid duplicates
-DO $$ BEGIN IF NOT EXISTS (
-    SELECT 1
-    FROM cron.job
-    WHERE command = 'SELECT handle_expired_deals()'
-) THEN PERFORM cron.schedule(
-    'handle_expired_deals_job',
-    '0 * * * *',
-    'SELECT handle_expired_deals()'
-);
-END IF;
-END $$;
+-- Schedule the function to run once a day at midnight
+SELECT cron.schedule(
+        'handle_expired_deals_job',
+        '0 0 * * *',
+        'SELECT handle_expired_deals()'
+    );
