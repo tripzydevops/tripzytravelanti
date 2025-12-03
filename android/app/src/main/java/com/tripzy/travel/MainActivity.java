ac2f853e45614dd6e695df024d83695659a86396
tripzy <tripzydevops@gmail.com>
2025-12-03 09:51:13 +0300

    Add mobile configuration and update dependencies

---
+++ android/app/src/main/java/com/tripzy/travel/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tripzy.travel;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tripzy.travel',
+  appName: 'Tripzy',
+  webDir: 'dist'
+};
+
+export default config;
