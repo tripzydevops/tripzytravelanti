cb3938a2f13eb745e59ce734fc1a4c66d14fa58e
tripzy <tripzydevops@gmail.com>
2025-12-08 16:55:20 +0300

    chore: add google project idx configuration

---
+++ .idx/dev.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx = {
+    extensions = [
+      "branding-brand.vscode-react-pro"
+      "bradlc.vscode-tailwindcss"
+      "esbenp.prettier-vscode"
+    ];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+    workspace = {
+      onCreate = {
+        install = "npm install";
+      };
+      onStart = {
+        # Nothing specific for now
+      };
+    };
+  };
+}
